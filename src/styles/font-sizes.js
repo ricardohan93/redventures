34986854c9c36c463dbefebd87a7ede03a38d59f
Ricardo <ricardoshhan@gmail.com>
2020-01-21 00:45:44 -0300

    adding custom styles

---
+++ src/styles/font-sizes.js
@@ -0,0 +1,13 @@
+export const tiny = "12px";
+export const small = "14px";
+export const normal = "16px";
+export const large = "18px";
+export const huge = "24px";
+
+export default {
+	tiny,
+	small,
+	normal,
+	large,
+	huge
+};
